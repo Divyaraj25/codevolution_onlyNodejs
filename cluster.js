decaebe61eb1b1fd0740dee6b51b36c0e0ffbdeb
Divyaraj Makwana <divyaraj.makwana9425@gmail.com>
2024-09-17 11:50:07 +0530

    updated cluster.js, printed process pid allocated

--- cluster.js
+++ cluster.js
@@ -1,5 +1,8 @@
 const cluster = require("node:cluster");
 const http = require("node:http");
+const os = require("node:os");
+
+console.log(`CPUs : ${os.cpus().length}`);
 
 if (cluster.isMaster) {
   console.log(`master : ${process.pid} is running`);
@@ -12,11 +15,11 @@ if (cluster.isMaster) {
     .createServer((req, res) => {
       if (req.url === "/") {
         res.writeHead(200, "ok", { "Content-Type": "text/plain" });
-        res.end("<h1>Home Page</h1>");
+        res.end(`<h1>Home Page ${process.pid}</h1>`);
       } else if (req.url === "/about") {
-        for (let i = 0; i < 600000000; i++) {}
+        for (let i = 0; i < 6000000000; i++) {}
         res.writeHead(200, "ok", { "Content-Type": "text/plain" });
-        res.end("<h1>About Page</h1>");
+        res.end(`<h1>About Page ${process.pid}</h1>`);
       }
     })
     .listen(3000, () => {
